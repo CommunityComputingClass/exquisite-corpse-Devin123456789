135e29db2a90510004545e624329ad49d8fc9453
Devin123456789 <163028284+Devin123456789@users.noreply.github.com>
2025-01-13 23:29:15 +0000

    Second submission

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,11 +1,18 @@
 function setup() {
-  createCanvas(900, 100);
+  createCanvas(600, 600);
+  eyeOfRah(100,200);
+  eyeOfRah(350, 200);
 }
 
 function draw() {
-  background(200);
-
 }
 
-
+function eyeOfRah(x,y) {
+   fill("orange")
+   ellipse(x + 100, y + 100, 80, 50);
+   fill("blue")
+   ellipse(x + 95, y + 100,30,30)
+  fill("orange")
+   circle(x + 92, y + 95, 10);
+}
 
