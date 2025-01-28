ef3a3c299237d22ed8fe534d5a91bc4cda0cdc66
Devin123456789 <163028284+Devin123456789@users.noreply.github.com>
2025-01-28 23:26:55 +0000

    Connect

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,56 +1,16 @@
-let faceMesh;
-let video;
-let faces = [];
-let options = { maxFaces: 2, refineLandmarks: false, flipHorizontal: false };
-
-function preload() {
-  // Load the faceMesh model
-  faceMesh = ml5.faceMesh(options);
-}
-
 function setup() {
-  createCanvas(640, 480);
-  // Create the webcam video and hide it
-  video = createCapture(VIDEO);
-  video.size(640, 480);
-  video.hide();
-  // Start detecting faces from the webcam video
-  faceMesh.detectStart(video, gotFaces);
+  createCanvas(400, 400);
 }
-
 function draw() {
-  // Draw the webcam video
-  image(video, 0, 0, width, height);
-
-  //Draw all the tracked face points
-  for (let i = 0; i < faces.length; i++) {
-    let face = faces[i];
-    
-    for (let j = 0; j < face.keypoints.length; j++) {
-      let keypoint = face.keypoints[j];
-     // console.log(keypoint)
-      fill(0, 255, 0);
-      noStroke();
-      circle(keypoint.x, keypoint.y, 5);
-      
-      eyeOfRah(face.keypoints[2].x, face.keypoints[2].y)
-    }
+  background(50);
+  strokeWeight(2);
+  fill(128 + sin(frameCount*0.1) * 128);
+  if (mouseIsPressed) {
+    stroke(255);
   }
-  
-
-}
-
-// Callback function for when faceMesh outputs data
-function gotFaces(results) {
-  // Save the output to the faces variable
-  faces = results;
-}
-
-function eyeOfRah(x,y) {
-   fill("orange")
-   ellipse(x + 100, y + 100, 80, 50);
-   fill("blue")
-   ellipse(x + 95, y + 100,30,30)
-  fill("orange")
-   circle(x + 92, y + 95, 10);
+  else {
+    noStroke();
+  }
+  textSize(12 + (mouseX / width)*72);
+  text("Connect, Connect, Connect", 50, 200);
 }
