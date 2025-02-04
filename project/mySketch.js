8d9728a37db64570499c5cd778fad7fd7495aa39
Devin123456789 <163028284+Devin123456789@users.noreply.github.com>
2025-02-04 20:05:31 +0000

    type

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,27 +1,14 @@
+let contents = "";
 function setup() {
   createCanvas(400, 400);
 }
 function draw() {
-  background(50);
-  strokeWeight(2);
-  fill(128 + sin(frameCount*0.1) * 128);
-  if (mouseIsPressed) {
-    stroke(255);
-  }
-  else {
-    noStroke();
-  }
-  textSize(12 + (mouseX / width)*72);
-  text("Connect, Connect, Connect", 50, 200);
+  background(200);
+  fill(255);
+  textSize(24);
+  text(contents, 50, 50, 300, 300);
+}
+function keyTyped() {
+  contents += key;
 }
 
-function KeyPressed(){
-  if (keyCode === 81){
-    WhiteSquare()
-  }
-}
-
-function WhiteSquare(){
-  Rect(30, 20, 55, 55);
-  fill('130,200,130');
-}
