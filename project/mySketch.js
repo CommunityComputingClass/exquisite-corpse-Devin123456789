dc8cb9e3f1982fcef30dfcf5c568f31ffd1c03db
Devin123456789 <163028284+Devin123456789@users.noreply.github.com>
2025-02-10 23:26:05 +0000

    weird ahh text box

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,14 +1,40 @@
+let myQuestion = "Do you have any pets?"
+ 
 let contents = "";
+
+let placeHolderText = "Enter your answer here..."
+
+let button
+let textBox; 
+
 function setup() {
-  createCanvas(400, 400);
-}
-function draw() {
-  background(200);
+  createCanvas(500, 400);
+  button = createButton("show");
+  button.position(220,80)
+  button.mousePressed(showText)
+  
+  textBox = createInput(placeHolderText)
+  input.style = ('color', 'grey')
+  textBox.size(300,40)
+  textBox.position(50,200)
+  textBox.input(storeText)
+  
+  background(50);
   fill(255);
-  textSize(24);
-  text(contents, 50, 50, 300, 300);
-}
-function keyTyped() {
-  contents += key;
+  textSize(14);
+  text(myQuestion,50,50) 
 }
 
+function draw() {
+  
+}
+
+function showText(){ 
+   text(contents,50,200)
+   textBox.hide(); 
+   
+}
+
+function storeText(){
+  contents = this.value(); 
+}
